6b052d8478c119150e7c6971f1369c0884a986be
Anna Moignard <amoignard.coder@gmail.com>
2021-02-13 20:26:05 +1100

    began the sign up form and put some of the important components in the application. I will work on actie storage and AWS as the next step of the signup profile

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,93 @@
 import React, { useState, useEffect } from "react";
 
 
-export function SignUp() {
+export function SignUp({ history }) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [about_me, setAboutMe] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
+
+  async function onFormSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/sign-up`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ user: { username, email, about_me, password } }),
+        }
+      );
+      if (response.status >= 400) {
+        throw new Error("incorrect credentials");
+      } else {
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}/sign-in`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ auth: { username, email, about_me, password, password_confirmation } }),
+          }
+        );
+        const { jwt } = await response.json();
+        localStorage.setItem("token", jwt);
+        history.push("/");
+      }
+    } catch (err) {}
+  }
+
   return (
     <>
+
       <h1>Sign Up</h1>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="username"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password_confirmation">Password Confirmation</label>
+          <input
+            type="password_confirmation"
+            name="password_confirmation"
+            id="password_confirmation"
+            value={password_confirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+          <label htmlFor="about_me">About Me</label>
+          <textarea
+            type="about_me"
+            name="about_me"
+            id="about_me"
+            value={about_me}
+            onChange={(e) => setAboutMe(e.target.value)}
+          />
+          <input id="submit" type="submit" value="Submit" />
+        </form>
     </>
-  )
+  );
 }
