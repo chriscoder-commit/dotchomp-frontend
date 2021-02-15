9e611b450184b023141505da8a2928171ecd2262
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-14 21:56:26 -0800

    begun form

--- src/components/NewRecipe.jsx
+++ src/components/NewRecipe.jsx
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-export function NewRecipe() {
+export function NewRecipe({ history }) {
+  const [ingredients, setIngredients] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [prep_time, setPreptime] = useState("");
+  const [cook_time, setCooktime] = useState("");
+  const [serves, setServes] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  async function onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
   <h1>Feed the Feed!</h1>
   )
 }
+
+
+// t.string "ingredients"
+// t.string "cuisine"
+// t.string "prep_time"
+// t.string "cook_time"
+// t.integer "serves"
+// t.string "photo"
